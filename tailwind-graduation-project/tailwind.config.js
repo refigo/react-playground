e03e2e7219f88d3530bf9c17d7b7e61f433dfb3f
Refi Go <42.4.mgo@gmail.com>
2025-04-21 01:51:51 +0900

    Refactor: Modify friend images

---
+++ tailwind-graduation-project/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.html',
+    './**/*.js',
+    './**/*.ts',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
